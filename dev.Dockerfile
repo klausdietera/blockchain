3718da2ab0260ed93db7009a1c33816244c11043
vicbeck2 <vic.beck@gmx.com>
2019-05-09 12:07:19 +0300

    Remove beego

--- dev.Dockerfile
+++ dev.Dockerfile
@@ -1,7 +1,6 @@
 FROM golang
 
 # Install beego & bee
-RUN go get github.com/astaxie/beego
 RUN go get github.com/beego/bee
 
 RUN mkdir -p /go/src/bitbucket.org/axelsheva/blockchain
--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	_ "bitbucket.org/axelsheva/blockchain/routers"
-	"github.com/astaxie/beego"
-)
-
 func main() {
-	if beego.BConfig.RunMode == "dev" {
-		beego.BConfig.WebConfig.DirectoryIndex = true
-		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
-	}
-	beego.Run()
+	println("Hello world!")
 }
